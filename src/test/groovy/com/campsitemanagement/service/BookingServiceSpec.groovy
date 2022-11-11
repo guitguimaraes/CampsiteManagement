260237cb228a77d531f99c450a8b7ebe0377362a
gguimar <guilherme.guimaraes@adp.com>
2022-11-11 16:13:11 -0300

    Adding some groovy tests

--- src/test/groovy/com/campsitemanagement/service/BookingServiceSpec.groovy
+++ src/test/groovy/com/campsitemanagement/service/BookingServiceSpec.groovy
@@ -1,9 +1,9 @@
 package com.campsitemanagement.service
 
 import com.campsitemanagement.entity.Booking
+import com.campsitemanagement.exception.BookingNotAvailableException
+import com.campsitemanagement.exception.NotFoundException
 import com.campsitemanagement.repository.BookingRepository
-import com.campsitemanagement.service.BookingService
-import com.campsitemanagement.service.BookingServiceImpl
 import spock.lang.Specification
 
 import java.time.LocalDate
@@ -34,6 +34,62 @@ class BookingServiceSpec extends Specification {
         noExceptionThrown()
     }
 
+    def 'Booking insert with Date not available'() {
+        given: 'correct booking'
+
+        Booking bookingInsert = createBooking()
+
+
+        def DatesList = [Mock(Booking), Mock(Booking)]
+        1 * bookingRepository.findByEndDateBetweenOrStartDateBetween(_, _, _, _) >> DatesList
+
+        when:
+
+        bookingService.addBooking(bookingInsert)
+
+        then:
+
+        thrown(BookingNotAvailableException)
+    }
+
+    def 'Booking delete with success'() {
+        given: 'correct bookingId to delete'
+
+        Booking bookingDeleted = createBooking()
+
+        1 * bookingRepository.findById('bookingId') >> Optional.of(bookingDeleted)
+        1 * bookingRepository.delete(bookingDeleted)
+
+        when:
+
+        bookingService.deleteBooking('bookingId')
+
+        then:
+
+        noExceptionThrown()
+    }
+
+    def 'Booking not found to delete'() {
+        given: 'incorrect bookingId to delete'
+
+
+        1 * bookingRepository.findById('bookingId') >> Optional.empty()
+
+        when:
+
+        bookingService.deleteBooking('bookingId')
+
+        then:
+
+        thrown(NotFoundException)
+    }
+
+
+   /** @Execution(value = ExecutionMode.CONCURRENT)
+    defTestparalel(){
+        'Booking insert with success'()
+    }
+*/
     def createBooking(){
         Booking booking = new Booking(
                 'id',
